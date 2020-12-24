9e7ea6a1a6918080f8c00f44be8a454be36614d3
Marlene <36805801+Mplasen1211@users.noreply.github.com>
2020-12-24 12:17:25 -0800

    Add files via upload

---
+++ Basic Projects/project10_loops_arrays/js/main.js
@@ -0,0 +1,38 @@
+// While Loop
+function count_To_Ten() {
+    var Digit = "";
+    var X = 1;
+    while (X < 11) {
+        Digit += "<br>" + X;
+        X++;
+    }
+    document.getElementById("Counting_to_Ten").innerHTML = Digit;
+}
+// For Loop
+var Instruments = ["Guitar", "Drums", "Piano", "Bass", "Violin", "Trumpet", "Flute"];
+var Content = "";
+var Y;
+function for_Loop() {
+    for (Y = 0; Y < Instruments.length; Y++) {
+        Content += Instruments[Y] + "<br>";
+    }
+    document.getElementById("List_of_Instruments").innerHTML = Content;
+}
+// Array
+function dog_pics() {
+    var Dog_Picture = [];
+    Dog_Picture[0] = "sleeping";
+    Dog_Picture[1] = "playing";
+    Dog_Picture[2] = "eating";
+    Dog_Picture[3] = "barking";
+    document.getElementById("Cat").innerHTML = "In this picture, the dog is " +
+        Dog_Picture[2] + ".";
+}
+// "Let" Keyword 
+var X = 82;
+document.write(X);
+{
+    let X = 33;
+    document.write("<br>" + X);
+}
+document.write("<br>" + X);
