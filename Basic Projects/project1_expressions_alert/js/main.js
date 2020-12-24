d8dcee66a1fbccd6935c24c8ce9243712a14ec4c
Marlene <36805801+Mplasen1211@users.noreply.github.com>
2020-12-23 19:37:38 -0800

    Add files via upload

--- Basic Projects/project1_expressions_alert/js/main.js
+++ Basic Projects/project1_expressions_alert/js/main.js
@@ -1,13 +1,22 @@
 var A = "I'm an aspiring developer.";
 var B = "Checkout my Java skills!";
+
 var B = B.fontcolor("pink");
+
 document.write(A);
 document.write(B);
 document.write("\"The more I study,"
 + "the more insatiable do I feel my genius for it to be.\""
 + "-Ada Lovelace");
+
 window.alert("Hello, World!");
 
+function My_First_Function() {
+    var str = "This text is green!";
+    var result = str.fontcolor("green");
+    document.getElementById("Green_Text").innerHTML = result;
+}
+//Utilizing functions to make my code more dynamic//
 
 
 
