54d92d88e0a5513193dc32d86f2323605830911c
Marlene <36805801+Mplasen1211@users.noreply.github.com>
2020-12-23 16:10:16 -0800

    Add files via upload

---
+++ Basic Projects/project1_expressions_alert/js/main.js
@@ -0,0 +1,13 @@
+var A = "I'm an aspiring developer.";
+var B = "Checkout my Java skills!";
+var B = B.fontcolor("pink");
+document.write(A);
+document.write(B);
+document.write("\"The more I study,"
++ "the more insatiable do I feel my genius for it to be.\""
++ "-Ada Lovelace");
+window.alert("Hello, World!");
+
+
+
+
