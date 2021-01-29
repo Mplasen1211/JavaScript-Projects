d441fe95a9af5a3e3490dceaffd805c388a632b9
Marlene <36805801+Mplasen1211@users.noreply.github.com>
2021-01-29 13:04:37 -0800

    Add files via upload

--- Projects/TicTacToe/js/tictactoe.js
+++ Projects/TicTacToe/js/tictactoe.js
@@ -72,35 +72,35 @@ function checkWinConditions() {
     // X 0, 1, 2 condition.
     if (arrayIncludes('0X', '1X', '2X')) {drawWinLine(50, 100, 558, 100); }
     // X 3, 4, 5 condition.
-    if (arrayIncludes('3X', '4X', '5X')) {drawWinLine(50, 304, 558, 304); }
+    else if (arrayIncludes('3X', '4X', '5X')) {drawWinLine(50, 304, 558, 304); }
     // X 6, 7, 8 condition.
-    if (arrayIncludes('6X', '7X', '8X')) {drawWinLine(50, 508, 558, 508); }
+    else if (arrayIncludes('6X', '7X', '8X')) {drawWinLine(50, 508, 558, 508); }
     // X 0, 3, 6 condition.
-    if (arrayIncludes('0X', '3X', '6X')) {drawWinLine(100, 50, 100, 558); }
+    else if (arrayIncludes('0X', '3X', '6X')) {drawWinLine(100, 50, 100, 558); }
     // X 1, 4, 7 condition.
-    if (arrayIncludes('1X', '4X', '7X')) {drawWinLine(304, 50, 304, 558); }
+    else if (arrayIncludes('1X', '4X', '7X')) {drawWinLine(304, 50, 304, 558); }
     // X 2, 5, 8 condition.
-    if (arrayIncludes('2X', '5X', '8X')) {drawWinLine(508, 50, 508, 558); }
+    else if (arrayIncludes('2X', '5X', '8X')) {drawWinLine(508, 50, 508, 558); }
     // X 6, 4, 2 condition.
-    if (arrayIncludes('6X', '4X', '2X')) {drawWinLine(100, 508, 510, 90); }
+    else if (arrayIncludes('6X', '4X', '2X')) {drawWinLine(100, 508, 510, 90); }
     // X 0, 4, 8 condition.
-    if (arrayIncludes('0X', '4X', '8X')) {drawWinLine(100, 100, 520, 520); }
+    else if (arrayIncludes('0X', '4X', '8X')) {drawWinLine(100, 100, 520, 520); }
     // O 0, 1, 2 condition.
-    if (arrayIncludes('0O', '1O', '2O')) {drawWinLine(50, 100, 558, 100); }
+    else if (arrayIncludes('0O', '1O', '2O')) {drawWinLine(50, 100, 558, 100); }
     // O 3, 4, 5 condition.
-    if (arrayIncludes('3O', '4O', '5O')) {drawWinLine(50, 304, 558, 304); }
+    else if (arrayIncludes('3O', '4O', '5O')) {drawWinLine(50, 304, 558, 304); }
     // O 6, 7, 8 condition.
-    if (arrayIncludes('6O', '7O', '8O')) {drawWinLine(50, 508, 558, 508); }
+    else if (arrayIncludes('6O', '7O', '8O')) {drawWinLine(50, 508, 558, 508); }
     // O 0, 3, 6 condition.
-    if (arrayIncludes('0O', '3O', '6O')) {drawWinLine(100, 50, 100, 558); }
+    else if (arrayIncludes('0O', '3O', '6O')) {drawWinLine(100, 50, 100, 558); }
     // O 1, 4, 7 condition.
-    if (arrayIncludes('1O', '4O', '7O')) {drawWinLine(304, 50, 304, 558); }
+    else if (arrayIncludes('1O', '4O', '7O')) {drawWinLine(304, 50, 304, 558); }
     // O 2, 5, 8 condition.
-    if (arrayIncludes('2O', '5O', '8O')) {drawWinLine(508, 50, 508, 558); }
+    else if (arrayIncludes('2O', '5O', '8O')) {drawWinLine(508, 50, 508, 558); }
     // O 6, 4, 2 condition.
-    if (arrayIncludes('6O', '4O', '2O')) {drawWinLine(100, 508, 510, 90); }
+    else if (arrayIncludes('6O', '4O', '2O')) {drawWinLine(100, 508, 510, 90); }
     // O 0, 4, 8 condition.
-    if (arrayIncludes('0O', '4O', '8O')) {drawWinLine(100, 100, 520, 520); }
+    else if (arrayIncludes('0O', '4O', '8O')) {drawWinLine(100, 100, 520, 520); }
     // This condition checks for tie. If none of the above conditions register,
     // and 9 squares are selected, the code executes.
     else if (selectedSquares.length >= 9) {
@@ -191,7 +191,7 @@ function checkWinConditions() {
         if (x1 <= x2 && y1 >= y2) {
             if (x < x2) { x += 10; }
             if (y > y2) { y -= 10; }
-            if (x >= x2 && y <= y2) {cancelAnimationFrame(animationLoop); }
+            if (x >= x2 && y <= y2) { cancelAnimationFrame(animationLoop); }
         }
     }
 
@@ -213,6 +213,8 @@ function checkWinConditions() {
     // This line  waits 1 second.
     // Then, clears canvas, resets game, and allows clicking again.
     setTimeout(function () { clear(); resetGame(); }, 1000);
+
+
 
 
 // This function resets the game in a tie or a win.
